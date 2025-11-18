1d1727f7b5c637fc42c997c4d55998f29280dada
Neky547 <nekenarab55@gmail.com>
2025-11-18 09:22:39 +0100

    Ajout controller, modification dashboard

--- src/main/kotlin/org/ldv/groovium/controller/AdminCategorieController.kt
+++ src/main/kotlin/org/ldv/groovium/controller/AdminCategorieController.kt
@@ -1,4 +1,18 @@
 package org.ldv.groovium.controller
 
-class AdminCategorieController {
+import org.ldv.groovium.model.dao.CategorieDAO
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class AdminCategorieController(val categorieDAO: CategorieDAO) {
+
+    @GetMapping("/Groovium/admin/categorie")
+    fun index(model: Model): String {
+        //On récupère les valeurs de chaque catégorie avec l'attribut "categories"
+        model.addAttribute("categories",categorieDAO.findAll())
+        return "pageAdmin/categorie/index"
+    }
+
 }
