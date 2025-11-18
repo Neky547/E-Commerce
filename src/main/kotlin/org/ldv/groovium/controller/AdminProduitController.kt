801399e8f2d385cdcc7c61bd1c9d9be2c4ae24d9
Neky547 <nekenarab55@gmail.com>
2025-11-18 11:36:10 +0100

    Gestion des articles

---
+++ src/main/kotlin/org/ldv/groovium/controller/AdminProduitController.kt
@@ -0,0 +1,15 @@
+package org.ldv.groovium.controller
+
+import org.ldv.groovium.model.dao.ProduitDAO
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class AdminProduitController (val produitDAO: ProduitDAO){
+    @GetMapping("/Groovium/admin/produit")
+    fun index(model: Model): String{
+        model.addAttribute("produits", produitDAO.findAll())
+        return "pageAdmin/produit/index"
+    }
+}
