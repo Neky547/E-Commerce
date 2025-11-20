887c935ca166b86208c1146b6ec3c84f6e3dec96
Neky547 <nekenarab55@gmail.com>
2025-11-20 12:01:21 +0100

    mapping du login

--- src/main/kotlin/org/ldv/groovium/controller/MainController.kt
+++ src/main/kotlin/org/ldv/groovium/controller/MainController.kt
@@ -1,7 +1,9 @@
 package org.ldv.groovium.controller
 
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
 
 @Controller
 class MainController {
@@ -34,4 +36,12 @@ class MainController {
     fun rgpd():String{
         return "pagesVisiteur/rgpd"
     }
+
+    @GetMapping("/Groovium/login")
+    fun login(@RequestParam error: Boolean?, model: Model): String {
+        // Ajoute un attribut "error" au modèle si la requête contient une erreur
+        model.addAttribute("error", error == true)
+        return "pagesVisiteur/login"
+    }
+
 }
