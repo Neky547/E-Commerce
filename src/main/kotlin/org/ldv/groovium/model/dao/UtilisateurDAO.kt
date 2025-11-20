32ce00342ca892efa02f439dad46db5a51cc7f31
Neky547 <nekenarab55@gmail.com>
2025-11-20 08:58:15 +0100

    Spring security

--- src/main/kotlin/org/ldv/groovium/model/dao/UtilisateurDAO.kt
+++ src/main/kotlin/org/ldv/groovium/model/dao/UtilisateurDAO.kt
@@ -1,7 +1,11 @@
 package org.ldv.groovium.model.dao
 import org.ldv.groovium.model.entity.Utilisateur
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 
 interface UtilisateurDAO : JpaRepository<Utilisateur, Long> {
+
+    @Query("select u from Utilisateur u where u.email = ?1")
+    fun findByEmail(email: String): Utilisateur
 
 }
