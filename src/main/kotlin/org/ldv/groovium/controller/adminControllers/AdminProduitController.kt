6831ce7511cb4bbd30b8de4fd3183828571c65a7
Neky547 <nekenarab55@gmail.com>
2025-11-24 12:00:30 +0100

    GetMapping création produit

--- src/main/kotlin/org/ldv/groovium/controller/adminControllers/AdminProduitController.kt
+++ src/main/kotlin/org/ldv/groovium/controller/adminControllers/AdminProduitController.kt
@@ -1,13 +1,15 @@
 package org.ldv.groovium.controller.adminControllers
 
+import org.ldv.groovium.model.dao.CategorieDAO
 import org.ldv.groovium.model.dao.ProduitDAO
+import org.ldv.groovium.model.entity.Produit
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 
 @Controller
-class AdminProduitController (val produitDAO: ProduitDAO){
+class AdminProduitController (val produitDAO: ProduitDAO, val categorieDAO: CategorieDAO){
     @GetMapping("/Groovium/admin/produit")
     fun index(model: Model): String{
         model.addAttribute("produits", produitDAO.findAll())
@@ -19,5 +21,15 @@ class AdminProduitController (val produitDAO: ProduitDAO){
         val unProduit = produitDAO.findById(id).orElseThrow()
         model.addAttribute("produit", unProduit)
         return "pageAdmin/produit/show"
+    }
+
+    //Méthode de création de produit
+    @GetMapping("Groovium/admin/produit/creer")
+    fun create(model: Model):String{
+        var nouveauProduit = Produit(nom = "", description = "", stock = 1, prix = 1.0, lienImage = "")
+        var categories = categorieDAO.findAll()
+        model.addAttribute("categories", categories)
+        model.addAttribute("produit", nouveauProduit)
+        return "pageAdmin/produit/create"
     }
 }
